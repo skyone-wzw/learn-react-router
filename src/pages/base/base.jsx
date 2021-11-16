6862de1b7c03d6ad47a7c040a5eaa725d2a61672
skyone <skyone.wzw@qq.com>
2021-11-16 19:50:57 +0800

    增加基本路由的注释

--- src/pages/base/base.jsx
+++ src/pages/base/base.jsx
@@ -14,7 +14,20 @@ function Base({ match }) {
                 </ul>
             </nav>
             <Switch>
-                <Route exact path={`${match.path}/user`} component={User}/>
+                <Route path={`${match.path}`} exact render={() => (
+                    <>
+                        <p>
+                            我理解的，基本路由，就是<code>&lt;Switch/></code>、
+                            <code>&lt;Route/></code>标签混合使用，<code>&lt;Link/></code>
+                            用来跳转，当然，这些元素都要使用<code>&lt;Router/></code>标签包含。
+                        </p>
+                        <p>
+                            活用<code>&lt;Switch/></code>和<code>&lt;Redirect/></code>
+                            可以实现404页面，这部分代码在<code>App.jsx</code>里。
+                        </p>
+                    </>
+                )}/>
+                <Route path={`${match.path}/user`} component={User}/>
                 <Route path={`${match.path}/detail`} component={Detail}/>
             </Switch>
         </>
